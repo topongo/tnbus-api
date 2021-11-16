7ebbc51bcbed260d8cdaec59ab543221406a2aae
topongo <lorenzo.bodini.private@gmail.com>
2021-11-16 17:34:47 +0100

    Major improvement and implementations ADD By class, used in TNBus.get macro-function ADD preloading options, for avoiding huge api calls every restart (for testing only!) MOD modified loading and inheritance system, also due to an api bug not fixable by me. MOD get methods CAN DO ANYTHING (cit. deltarune), get now takes the type to return, and a series of filters, it will return all the objects contained in the TNBus object matching ALL the filters (so it's an AND and not an OR). If asked an element By.ID get will automatically strip it from the list. (NOTE: searching for ID doesn't always return a unique element (I swear it's not my fault), due to an api structural (in my opinion) problem) MOD get_* modified according to the new get function, the behaviour should be the same. ADD _load_trip() but not yet implemented ADD to each object (Area, Route, Stop, Trip) a dict that associate each search criterion to an object property. ADD to each object a __str__ and __repr__ function for better visualization. ADD stops property to Route and Area, for holding every stop onto a Route/Area MOD for Route object "type" index refers to the bus/train/cableway differentiation, while "routeType" index refers to "U" urban, "E" extra-urban routes. Adjust Route accordingly ADD areas property to Stop, representing which areas the stop intersects MOD Stop id is unreliable, because it's unique only in its subcategory urban or extra-urban. Now for id is "stopCode" (which is truly unique), and id_numeric is "stopId" ADD trips property to Stop, representing the trips that will encounter/had encountered that Stop. Added but not implemented MOD changed some Trip's property names MOD removed basic authentication key MOD API methods, add more options and failsafe features ADD trips_new to API, used to retrive Trips for a specific Route or Stop ADD type constants to Route

---
+++ tnbus-api/__init__.py
@@ -0,0 +1,3 @@
+from .tnbus_api import TNBus
+from .tnbus_api import API
+from .tnbus_api import By
--- tnbus-api/tnbus_api.py
+++ tnbus-api/tnbus_api.py
@@ -1,73 +1,179 @@
 import json
 import requests
+from unicodedata import normalize as u_normalize, combining as u_combining
+from datetime import datetime
+
+
+def remove_accents(inp):
+    return u"".join((c for c in u_normalize("NFKD", inp) if not u_combining(c)))
 
 
 class By:
+    # must refer to an int
     ID = 0
+    DIRECTION = 7
+
+    # must refer to a str
     NAME = 1
-    ROUTE = 2
-    STOP = 3
+    NAME_MATCH = 2
+
+    # must refer to a str or a int
+    TYPE = 6
+
+    # must refer to a TNBus.*
+    ROUTE = 3
+    STOP = 4
+    AREA = 5
+    TRIP = 8
+
+    # must refer to a datetime
+    MAX_DATE = 9
+    MIN_DATE = 10
+    STATE = 11
+
+    def string(self, _int):
+        for _d in self.__dir__():
+            if self.__getattribute__(_d) == _int:
+                return f"By.{_d}"
+
 
 class TNBus:
-    def __init__(self, _api):
+    def __init__(self, _api, preload=None):
         self.api = _api
-        stops = self.api.stops()
         self.areas = []
         self.news = []
-
-        areas_ids = []
         self.routes = []
         self.stops = []
-        for s in stops:
-            self.stops.append(TNBus.Stop(s))
-            for r in s["routes"]:
-                print(r)
-                g_r = self.get_route(r["routeId"])
-                g_a = self.get_area(r["areaId"])
-                if g_r is None:
-                    if g_a is None:
-                        self.areas.append(self.Area(r["areaId"]))
-                        _a = self.areas[-1]
-                    else:
-                        _a = g_a
-                    self.routes.append(self.Route(r, _a))
-                    _r = self.routes[-1]
-                else:
-                    _r = g_r
 
-                if isinstance(self.routes[-1].news, dict):
-                    self.news += self.routes[-1].news
-                self.stops[-1].routes.append(_r)
-
-        print(json.dumps(_api.trip_news(self.stops[50]), indent=4))
-
-    def get(self, _type, value, by=By.ID):
-        if _type is self.Stop:
-            store = self.stops
-        elif _type is self.Area:
-            store = self.areas
-        elif _type is self.Route:
-            store = self.routes
+        if not preload:
+            self.raw = {
+                "areas": self.api.areas(),
+                "routes": self.api.routes(),
+                "stops": self.api.stops(),
+                "trips": []
+            }
         else:
-            raise TypeError(f"Unrecognised type: {_type}")
-        if by == By.ID:
-            for _i in store:
-                if _i.id == value:
-                    return _i
+            self.raw = preload
+
+        for a in self.raw["areas"]:
+            if self.get_area(a["areaId"]) is None:
+                self.areas.append(self.Area(a))
+
+        # API.areas() doesn't return area n. 8, area of cable ways (only one actually)
+        self.areas.append(self.Area({"areaId": 8, "areaDesc": "Funivie", "type": "E"}))
+
+        """
+        # all stop dicts returned by API.stops are set to area 0
+        # this is an EXTERNAL bug, not fixable by me.
+        # So a separate call to API.routes is needed, even if API.stops returns stop info as well
+        """
+
+        for r in self.raw["routes"]:
+            self.routes.append(self.Route(r, self.get_area(r["areaId"])))
+            if type(self.routes[-1].news) is list:
+                self.news += self.routes[-1].news
+
+        for s in self.raw["stops"]:
+            self.stops.append(self.Stop(s))
+            for r in s["routes"]:
+                _r = self.get(self.Route, {By.ID: r["routeId"], By.TYPE: r["type"]})
+                self.stops[-1].routes.append(_r)
+                _r.stops.add(self.stops[-1])
+                self.stops[-1].areas.add(_r.area)
+                if self.stops[-1].routes[-1] is None:
+                    raise
+
+        if preload:
+            self.age = datetime.fromtimestamp(self.raw["age"])
+        else:
+            self.age = datetime.now()
+
+    def get(self, t_, filters: dict[int, any]):
+        for _b in filters:
+            if _b not in t_.SEARCH_ASSOC:
+                raise TypeError(f"Type {t_} doesn't support searches by {By.string(By(), _b)}")
+
+        store = self.__getattribute__(t_.SEARCH_STORE)
+
+        out = []
+        if store:
+            _iter = []
+            for _b, _v in filters.items():
+                try:
+                    if _v in store[0].__getattribute__(t_.SEARCH_ASSOC[_b]):
+                        pass
+                    _iter.append(True)
+                except TypeError:
+                    _iter.append(False)
+
+            for _s in store:
+                _add = True
+                for (_b, _v), _i in zip(filters.items(), _iter):
+                    s_target = _s.__getattribute__(t_.SEARCH_ASSOC[_b])
+                    if _i:
+                        if _b == By.NAME_MATCH:
+                            if remove_accents(_v.lower()) not in \
+                                    remove_accents(s_target.lower()):
+                                _add = False
+                        else:
+                            if _v not in s_target:
+                                _add = False
+                    else:
+                        if _v != s_target:
+                            _add = False
+                if _add:
+                    out.append(_s)
+
+        if By.ID in filters:
+            if len(out) == 1:
+                return out[0]
+            elif len(out) == 0:
+                return
+            else:
+                raise Exception(out)
+
+        return out
 
     def get_stop(self, value, by=By.ID):
-        return self.get(self.Stop, value, by)
+        return self.get(self.Stop, {by: value})
 
     def get_route(self, value, by=By.ID):
-        return self.get(self.Route, value, by)
+        return self.get(self.Route, {by: value})
 
     def get_area(self, value, by=By.ID):
-        return self.get(self.Area, value, by)
+        return self.get(self.Area, {by: value})
+
+    def _load_trip(self, trip_id: str):
+        pass
+
+    def dump(self, f_hand):
+        self.raw["age"] = self.age.timestamp()
+        json.dump(self.raw, f_hand)
 
     class Area:
-        def __init__(self, _id):
-            self.id = _id
+        SEARCH_ASSOC = {
+            By.ID: "id",
+            By.NAME: "desc",
+            By.NAME_MATCH: "desc",
+            By.ROUTE: "routes",
+            By.AREA: "",
+            By.TYPE: "type"
+        }
+        SEARCH_STORE = "areas"
+
+        def __init__(self, data):
+            self.id = data["areaId"]
+            self.desc = data["areaDesc"]
+            self.type = data["type"]
             self.routes = []
+            self.stops = []
+            self.raw = data
+
+        def __str__(self):
+            return f"Area(id={self.id},desc=\"{self.desc}\",type={self.type})"
+
+        def __repr__(self):
+            return self.__str__()
 
         class New:
             def __init__(self, data, route, area):
@@ -86,25 +192,66 @@ class TNBus:
                 self.raw = data
 
     class Route:
+        SEARCH_ASSOC = {
+            By.ID: "id",
+            By.NAME: "short_name",
+            By.NAME_MATCH: "short_name",
+            By.ROUTE: "",
+            By.AREA: "area",
+            By.STOP: "stops",
+            By.TYPE: "type",
+            By.TRIP: "trips"
+        }
+        SEARCH_STORE = "routes"
+        SEARCH_TRIPS_UPDATED = "trips_load"
+
+        TRAIN = 2
+        BUS = 3
+        CABLEWAY = 5
+
         def __init__(self, data, area):
             self.area = area
             self.area.routes.append(self)
+            self.stops = set()
             self.id = data["routeId"]
             self.news = [TNBus.Area.New(i, self, area) for i in data["news"]] if data["news"] is not None else None
             self.color = data["routeColor"]
             self.long_name = data["routeLongName"]
             self.short_name = data["routeShortName"]
-            self.type = data["routeType"]
-            # unknown usage of data at index "type"
-            # self.type = data["type"]
+            self.type = data["type"]
+            self.urban = True if data["routeType"] == "U" else False
+
+            self.trips = []
+            self.trips_load = datetime.fromtimestamp(0)
+
             self.raw = data
 
+        def __str__(self):
+            return f"Route(id={self.id},code=\"{self.short_name}\",name=\"{self.long_name}\")"
+
+        def __repr__(self):
+            return self.__str__()
+
     class Stop:
+        SEARCH_ASSOC = {
+            By.ID: "id",
+            By.NAME: "name",
+            By.NAME_MATCH: "name",
+            By.ROUTE: "routes",
+            By.AREA: "areas",
+            By.TYPE: "type",
+            By.TRIP: "trips"
+        }
+        SEARCH_STORE = "stops"
+        SEARCH_TRIPS_UPDATED = "trips_load"
+
         def __init__(self, data):
             self.routes = []
-            self.code = data["stopCode"]
+            self.areas = set()
+            self.id = data["stopCode"]
             self.desc = data["stopDesc"]
-            self.id = data["stopId"]
+            # id_numeric is UNRELAIABLE!
+            self.id_numeric = data["stopId"]
             self.lat = data["stopLat"]
             self.level = data["stopLevel"]
             self.lon = data["stopLon"]
@@ -114,10 +261,43 @@ class TNBus:
             self.type = data["type"]
             self.wheelchair_boarding = data["wheelchairBoarding"]
 
-    class Trip:
+            self.trips = []
+            self.trips_load = datetime.fromtimestamp(0)
+
+            self.raw = data
+
+        def __str__(self):
+            return f"Stop(id={self.id},n_id={self.id_numeric},name=\"{self.name}\")"
+
+        def __repr__(self):
+            return self.__str__()
+
+    class TripStopTime:
         def __init__(self, data):
+            pass
+
+    class Trip:
+        SEARCH_ASSOC = {
+            By.ROUTE: "routes",
+            By.STOP: "stops",
+            # search trip by id is very limiting!
+            By.ID: "id",
+            By.TRIP: "",
+            By.DIRECTION: "direction",
+            By.MAX_DATE: "actual_arrive_time",
+            By.MIN_DATE: "",
+            By.STATE: ""
+        }
+
+        NOT_DEPARTED = 0
+        DEPARTED = 1
+        ARRIVED = 2
+
+        def __init__(self, data, route):
+            # if not isinstance(route, TNBus.Route):
+            #    raise TypeError(f"\"route\" argument must be of type TNBus.Route, {type(route).__str__} given.")
             self.id = data["tripId"]
-            self.cableway = data["cableway"]
+            self.cable_way = data["cableway"]
             self.best = data["corsaPiuVicinaADataRiferimento"]
             self.delay = data["delay"]
             self.direction = data["directionId"]
@@ -125,17 +305,27 @@ class TNBus:
             self.last_sync = data["lastEventRecivedAt"]
             self.last_sequence_detection = data["lastSequenceDetection"]
             self.bus = TNBus.Bus(data["matricolaBus"])
-            self.ora_arrivo_effettiva_a_fermata_selezionata = data["oraArrivoEffettivaAFermataSelezionata"]
-            self.ora_arrivo_programmata_a_fermata_selezionata = data["oraArrivoProgrammataAFermataSelezionata"]
-            self.id = data["routeId"]
-            self.last = data["stopLast"]
+            self.actual_arrive_time = datetime.fromtimestamp(data["oraArrivoEffettivaAFermataSelezionata"])
+            self.scheduled_arrive_time = datetime.fromtimestamp(data["oraArrivoProgrammataAFermataSelezionata"])
+            self.route = route
+            self.last = None if data["stopLast"] == 0 else route.get(data["stopLast"])
             self.next = data["stopNext"]
             self.times = data["stopTimes"]
             self.totale_corse_in_lista = data["totaleCorseInLista"]
-            self.trip_flag = data["tripFlag"]
+
+            # TODO: finish compiling this case
+            if data["tripFlag"] == "TRIP_FLAG__MID":
+                self.state = self.DEPARTED
+            elif data["tripFlag"] == "":
+                self.state = self.ARRIVED
+            else:
+                self.state = self.NOT_DEPARTED
             self.trip_headsign = data["tripHeadsign"]
             self.type = data["type"]
             self.wheelchair_accessible = data["wheelchairAccessible"]
+
+        # def __str__(self):
+        #    return f"Route \"{self.short_name}\" - {self.long_name}"
 
     class Bus:
         def __init__(self, _id):
@@ -143,42 +333,58 @@ class TNBus:
 
 
 class API:
-    HEAD = {"Authorization": "Basic bWl0dG1vYmlsZTplY0dzcC5SSEIz"}
     URL = "https://app-tpl.tndigit.it/gtlservice"
 
-    def __init__(self):
-        pass
+    def __init__(self, key, preload=None):
+        self.auth = {"Authorization": f"Basic {key}"}
 
     def query(self, call, para=None):
         if para is None:
             para = {}
         return requests.get(f"{self.URL}/{call}",
                             params=para,
-                            headers=self.HEAD
+                            headers=self.auth
                             ).text
 
-    def routes(self, areas=()):
-        if not isinstance(areas, (tuple, list)):
-            raise TypeError("argument areas must be tuple or list")
-        return json.loads(self.query("routes", {"areas": ",".join(areas)} if areas else {}))
+    def routes(self, areas=None):
+        if areas is not None:
+            areas = ",".join(areas)
+        return json.loads(self.query("routes", {"areas": areas}))
 
-    def stops(self):
-        return json.loads(self.query("stops"))
+    def stops(self, areas=None):
+        if areas is not None:
+            areas = ",".join(areas)
+        return json.loads(self.query("stops", {"areas": areas}))
 
-    def trip(self, trip: TNBus.Trip):
-        return json.loads(self.query(f"trips/{trip.id}"))
+    def areas(self):
+        return json.loads(self.query("areas"))
 
-    def trip_news(self, stop: TNBus.Stop, limit=30):
-        return json.loads(self.query("trips_new", {"limit": limit, "stopId": stop.id, "type": stop.type}))
+    def trip(self, trip: str):
+        return json.loads(self.query(f"trips/{trip}"))
+
+    def trips_new(self, search: (TNBus.Stop, TNBus.Route), time=None, limit=30):
+        args = {
+            "limit": limit,
+            "type": search.type,
+            "refDateTime": time.isoformat() if isinstance(time, datetime) else None
+        }
+        if isinstance(search, TNBus.Stop):
+            args["stopId"] = search.id_numeric
+        elif isinstance(search, TNBus.Route):
+            args["routeId"] = search.id
+        else:
+            raise TypeError(f"{search} is of type {type(search)}")
+        return json.loads(self.query("trips_new", args))
 
 
 if __name__ == "__main__":
-    t = TNBus(API())
+    with open("auth") as f, open("data.json") as d:
+        t = TNBus(API(f.read().strip()), preload=json.load(d))
 
-    for i in t.routes:
-        if i.short_name == "13":
-            r = i
-    for i in t.stops:
-        if r.id in map(lambda l: l.id, i.routes):
-            if "Fogazzaro" in i.name:
-                print(t.api.trip_news(i)[0])
+    print(t.api.trip_news(t.get(
+        TNBus.Route,
+        {
+            By.STOP: t.get(TNBus.Stop, {By.NAME_MATCH: "povo sale"})[0],
+            By.NAME_MATCH: "5"
+        }
+    )[0]))
